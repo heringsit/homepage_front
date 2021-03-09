b907d00de21681e4f7a064798056964f59970b37
lhh0704 <hlh1783@naver.com>
2021-03-09 15:57:03 +0900

    로고 이미지 import (PartnersAinvestors)

--- src/views/Aboutus/Sections/PartnersAInvestors.js
+++ src/views/Aboutus/Sections/PartnersAInvestors.js
@@ -14,6 +14,7 @@ import i_logo_01 from "../../../assets/images/02about_investors/01alpen_n.svg";
 import i_logo_02 from "../../../assets/images/02about_investors/02korea_n.svg";
 import i_logo_03 from "../../../assets/images/02about_investors/03venture_n.svg";
 import i_logo_04 from "../../../assets/images/02about_investors/04cap_n.svg";
+import i_logo_05 from "../../../assets/images/02about_investors/05coree.png";
 
 export default function PartnersAInvestors(props) {
   return (
@@ -96,6 +97,9 @@ export default function PartnersAInvestors(props) {
             </div>
             <div className="inventors__image__container">
               <img src={i_logo_04} alt="cap" />
+            </div>
+            <div className="inventors__image__container">
+              <img src={i_logo_05} alt="coree" />
             </div>
           </div>
         </div>
