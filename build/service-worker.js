5d50bb78e5949e950404ae30ad579074300852a6
dev inki <inki.jo@heringsglobal.com>
2021-06-01 10:45:30 +0900

    new favicon

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.210e0d784e0314c175322a7b032afba1.js"
+  "/precache-manifest.3e8c8bbc9ece2bb8068ec3e6e581a151.js"
 );
 
 self.addEventListener('message', (event) => {
