498746244340c71c75bd8479e4a6a87cf2b60691
inkijo <woorim0ju@naver.com>
2022-03-29 10:09:29 +0900

    플랫폼에 영상 추가

--- src/views/Platforms/Platforms.js
+++ src/views/Platforms/Platforms.js
@@ -12,7 +12,6 @@ import Craimon from "./Sections/Craimon";
 import Rhexium from "./Sections/Rhexium";
 import RhexiumContents from "./Sections/RhexiumContents";
 import platformTopImage from "../../assets/images/03craimon/PLATFORMS_TOP.svg";
-
 import "./Platforms.css";
 
 export default function Platforms({ match }) {
@@ -24,6 +23,11 @@ export default function Platforms({ match }) {
       <Totop />
       <div id="platforms">
         <ContentsTitle matches={matches} title={"PLATFORMS"} />
+        <div className="SectionDivNT SectionDivUpBlank">
+          <video width="100%" controls autoPlay loop playsInline className="platform-video">
+            <source src={'https://s3.us-west-2.amazonaws.com/secure.notion-static.com/f5a23d3a-6c68-43d6-a263-0683525bd198/VD%ED%97%A4%EB%A7%81%EC%8A%A4220308_ENG%28no_sub%29.mp4?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20220323%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20220323T004358Z&X-Amz-Expires=86400&X-Amz-Signature=7f50a692792c411ed3035f12a6d4d32ccfdaf379bf9838a84c32f21164ec59ec&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22%255BVD%255D%255B%25ED%2597%25A4%25EB%25A7%2581%25EC%258A%25A4%255D220308_ENG%28no%2520sub%29.mp4%22&x-id=GetObject'} type="video/mp4" ></source>
+          </video>
+        </div>
         <div className="SectionDivNT SectionDivUpBlank">
           <div className="platformTopImage">
             <img
@@ -41,3 +45,4 @@ export default function Platforms({ match }) {
     </div>
   );
 }
+
