1b638c3e006d82262f3e110a24fcb8b8f32e352d
ramrami-12 <wislely66@gmail.com>
2022-08-16 14:17:10 +0900

    privacy buttons

---
+++ src/views/Privacy/Compoments/PrivacyButtons.js
@@ -0,0 +1,31 @@
+// Privacy & Term Buttons
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function PrivacyButtons({ texts, onClick }) {
+  const patharray = window.location.pathname.split("/");
+  const submenu = patharray[patharray.length - 1];
+  
+  return (
+    <div className="buttons">
+      {texts.map((button, idx) => (
+        <Link
+          className="no-decoration"
+          onClick={(e) => onClick(idx, e)}
+          to={`${idx}`}
+          key={idx}
+        >
+          {submenu === idx.toString() ? (
+            <div className="bg-button-selected policyButton FontR textF14 tcb">
+              {button}
+            </div>
+          ) : (
+            <div className="bg-button policyButton FontR textF14 tcb">
+              {button}
+            </div>
+          )}
+        </Link>
+      ))}
+    </div>
+  );
+}
