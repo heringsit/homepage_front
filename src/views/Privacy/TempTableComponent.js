13b848fa79f5629871daa68852dd66d36b29136a
Big-Arm <dukuu0405@gmail.com>
2022-08-16 15:32:35 +0900

    test table component without css 수정

--- src/views/Privacy/TempTableComponent.js
+++ src/views/Privacy/TempTableComponent.js
@@ -1,55 +1,68 @@
 import React from "react";
-const testTitle = ["구분", "항목", "기간"]
+const testTitle = ["구분", "항목", "기간"];
 const testVariables = [
-  "5. 대출 \n - 주택담보대출 찾기: 주택담보대출 찾기 서비스 제공 \n - 대출 받기: 대출 받기 서비스 제공 \n - 아파트 대출 한도 계산기: 주택담보대출 한도를 계산하여 정보 제공",
-  "5. 대출 \n\t - 주택담보대출 찾기: 주택담보대출 찾기 서비스 제공 ",
-  "5. 대출 \n - 주택담보대출 찾기: 주택담보대출 찾기 서비스 제공",
-]
+  [
+    "5. 대출 \n - 주택담보대출 찾기: 주택담보대출 찾기 서비스 제공 \n - 대출 받기: 대출 받기 서비스 제공 \n - 아파트 대출 한도 계산기: 주택담보대출 한도를 계산하여 정보 제공",
+    "5. 대출 \n\t - 주택담보대출 찾기: 주택담보대출 찾기 서비스 제공 ",
+    "5. 대출 \n - 주택담보대출 찾기: 주택담보대출 찾기 서비스 제공",
+  ],
+  [
+    "5. 대출 \n - 주택담보대출 찾기: 주택담보대출 찾기 서비스 제공 \n - 대출 받기: 대출 받기 서비스 제공 \n - 아파트 대출 한도 계산기: 주택담보대출 한도를 계산하여 정보 제공",
+    "5. 대출 \n\t - 주택담보대출 찾기: 주택담보대출 찾기 서비스 제공 ",
+    "5. 대출 \n - 주택담보대출 찾기: 주택담보대출 찾기 서비스 제공",
+  ],
+];
 const newLine = new RegExp("\n");
 const newTab = new RegExp("\t");
-const Table =  ({titles=testTitle, contents=testVariables}) => {
+const Table = ({ titles = testTitle, cell = testVariables }) => {
   const processNewLine = (content) => {
-    return (content.split(newLine))
-  }
+    return content.split(newLine);
+  };
   const processTab = (content, idx) => {
-
     console.log(newTab.test(content));
     if (newTab.test(content)) {
-      return(<p className="tab-1" key={idx}>
-        {content}
-      </p>)
-    }
-    else return(<p key={idx}>
-    {content}
-  </p>)
-  }
+      return (
+        <p className="tab-1" key={idx}>
+          {content}
+        </p>
+      );
+    } else return <p key={idx}>{content}</p>;
+  };
   return (
     <table cellSpacing={0}>
       <thead>
+        <tr>
+          {titles.map((title, idx) => (
+            <th
+              key={idx}
+              className={`border border-b-0 ${
+                idx === titles.length - 1 ? "" : "border-r-0"
+              }`}
+            >
+              {title}
+            </th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {cell.map((contents, cell_idx) => (
           <tr>
-            {titles.map((title, idx) => (
-              <th
-                key={idx}
-              >
-                {title}
-              </th>
-            ))}
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            {contents.map((content, idx) => (
+            {contents.map((content, contents_idx) => (
               <td
-                key={idx}
+                key={contents_idx}
+                className={`border ${
+                  cell_idx === cell.length - 1 ? "" : "border-b-0"
+                } ${contents_idx === titles.length - 1 ? "" : "border-r-0"}`}
               >
-                {processNewLine(content).map((line, idx) => 
-                  processTab(line, idx)
+                {processNewLine(content).map((line, contents_idx) =>
+                  processTab(line, contents_idx)
                 )}
               </td>
             ))}
           </tr>
-        </tbody>
+        ))}
+      </tbody>
     </table>
   );
-}
+};
 export default Table;
