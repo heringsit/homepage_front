6d1261ce87d17ce98a8acfbfc356e62e0a2aa962
memeoo <memehsforever@gmail.com>
2022-09-08 14:13:45 +0900

    add handleException #2

--- src/views/ham/Ham.js
+++ src/views/ham/Ham.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext } from "react";
+import React, { useState, useEffect, useContext, useRef } from "react";
 import axios from "axios";
 import moment from "moment";
 import useMediaQuery from "@material-ui/core/useMediaQuery";
@@ -7,22 +7,21 @@ import "./Ham.css";
 
 const Ham = () => {
   const [todayIndex, setTodayIndex] = useState(-1);
-  const [sabun, setSabun] = useState("");
-  const [clickedValue, setClickedValue] = useState("");
-  const [clickedId, setClickedId] = useState(-1);
-  
+  const [sabun, setSabun] = useState("");  
   const [exYoil, setExYoil] = useState([]);
   const [exType, setExType] = useState([]);
-
+  const [exceptionHandlingData, setExceptionHandlingData] = useState([]);
   const [days, setDays] = useState([]);
   const [workHistory, setWorkHistory] = useState([]);
   const matches = useMediaQuery("(max-width:600px)");
   let today = moment().format("YY-MM-DD");
   const [isShowAttendanceBoard, setIsShowAttendanceBoard] = useState(false);
   const [userName, setUserName] = useState({name: "", sabun: window.localStorage.getItem("sabun")});
-  let timeGapArray = [];
-  // const BACK_END_URL = "http://52.79.53.196:3400";
-  const BACK_END_URL = "http://localhost:3400";
+  const [timeGapArray, setTimeGapArray] = useState([]);
+  const resettingRef = useRef(false);
+
+  const BACK_END_URL = "http://52.79.53.196:3400";
+  // const BACK_END_URL = "http://localhost:3400";
   const get5days = () => {
     let fiveDays = [];
     if (moment().day() == 1) {
@@ -62,6 +61,7 @@ const Ham = () => {
     return fiveDays;
   };
 
+
   const getCoreTimeViolationStamp = async (info) => {
     let result = await axios.post(BACK_END_URL+"/main/coretimeViolation", info);
     console.log("timeStamp >> ", result.data);
@@ -84,18 +84,8 @@ const Ham = () => {
            return <td class="rowTimeStamp" bgcolor={todayIndex == index+1 ? "#E6FFE6" : ""}>{endTime}</td>
          }else if(gubun == "over"){
           if(startTime != "기록없음" && endTime != "기록없음"){
-            let startHour = Number(startTime.split(":")[0]);
-            let startMinute = Number(startTime.split(":")[1]);
-            let endHour = Number(endTime.split(":")[0]);
-            let endMinute = Number(endTime.split(":")[1]);
-
-            let totalStartMin = startHour * 60 + startMinute;
-            let totalEndMin = endHour * 60 + endMinute;
-            let timeGap = totalEndMin - totalStartMin -540;
-            timeGapArray.push(timeGap);
-            return <td class="rowTimeStamp" bgcolor={todayIndex == index+1 ? "#E6FFE6" : ""}><span className="timeGapFont">{timeGap > 0 ? "+"+timeGap : timeGap}</span></td>
+            return <td class="rowTimeStamp" bgcolor={todayIndex == index+1 ? "#E6FFE6" : ""}><span className="timeGapFont">{timeGapArray[index] > 0 ? "+"+timeGapArray[index] : timeGapArray[index]}</span></td>
           }else{
-            timeGapArray.push("X");
             return <td class="rowTimeStamp" bgcolor={todayIndex == index+1 ? "#E6FFE6" : ""}>X</td>
           }
 
@@ -105,15 +95,17 @@ const Ham = () => {
   
   const getAccTime = () => {
     let tArr = [];
+    let sum = 0;
     for(let index=0; index < timeGapArray.length ; index++){
       if(timeGapArray[index] == "X"){
         tArr.push(<td colSpan={5-index} class="rowTimeStamp" bgcolor={todayIndex == index+1 ? "#E6FFE6" : ""}><span className="exceptionFont">근태예외처리필요</span></td>);
         break;
       }else{
         if(index == 0){
+          sum = timeGapArray[0]; 
           tArr.push(<td class="rowTimeStamp" bgcolor={todayIndex == index+1 ? "#E6FFE6" : ""}><span className="timeGapFont">{timeGapArray[index] > 0 ? "+"+timeGapArray[index]:timeGapArray[index]}</span></td>)
         }else{
-          let sum = timeGapArray[index] + timeGapArray[index-1];
+          sum = sum + timeGapArray[index];
           tArr.push(<td class="rowTimeStamp" bgcolor={todayIndex == index+1 ? "#E6FFE6" : ""}><span className="timeGapFont">{sum > 0 ? "+"+sum : sum}</span></td>)
         }
       }
@@ -135,54 +127,24 @@ const Ham = () => {
   }
   
   useEffect(() => {
+    console.log("aaaaa");
     if(window.localStorage.getItem("sabun")){    // 등록한 사번이 있으면, -> 출근부 보여줌
       setIsShowAttendanceBoard(true);
     }else{
       setIsShowAttendanceBoard(false);           // 사번이 없으면 -> 등록
     }
 
-    let todayInfo = {
-      sabun: window.localStorage.getItem("sabun"),
-      today: -1,
-      yoil: moment().day()
-    };
-
     setDays(get5days());
     setTodayIndex(moment().day());
 
-    const getWeekHistory = async () => {
-      let result = await axios.post(BACK_END_URL+"/main/workhistory",todayInfo);
-      console.log("result >> ", result.data);
-      setUserName({name:result.data.weekHistory[0].name, sabun: window.localStorage.getItem("sabun")});
-      let wswc = [];
-      let history = result.data.weekHistory;   
-      for(let index = 0 ; index < history.length; index++){
-        let element = {wstime:"", wctime:"", workdate:""};
-        if(history[index].wstime){  
-          element.wstime = transferPrettyTimeFormatted(getTimeOnly(history[index].wstime));
-        }else{
-          element.wstime = await getCoreTimeViolationStamp({name:history[0].name, theDay:history[index].workdate, violationType:"start"});
-        }
-        if(history[index].wctime){
-          element.wctime = transferPrettyTimeFormatted(getTimeOnly(history[index].wctime));
-        }else{
-          element.wctime = await getCoreTimeViolationStamp({name:history[0].name, theDay:history[index].workdate, violationType:"end"});
-        }
-        element.workdate = history[index].workdate;
-        wswc.push(element);
-        wswc.reverse();
-      }
-      setWorkHistory(wswc);
-    }
-    getWeekHistory();
-    
     const getExceptionHandling = async () => {
+      console.log("ddddddddddddddd")
       const handlingInfo = {
         todayYoil: moment().day(),
         sabun: userName.sabun
       }
+
       let result = await axios.post(BACK_END_URL+"/main/getExceptionHandling",handlingInfo);
-      console.log(" getExceptionHandling > ", result.data)
       let exYoil = result.data.exceptionList.map(each => {
         return moment(each.workdate).day()-1;
       });
@@ -190,13 +152,91 @@ const Ham = () => {
       let exType = result.data.exceptionList.map(each => {
         return each.handlingException;
       });
-
+      
       setExYoil(exYoil);
       setExType(exType);
+      resettingRef.current = true;
+      setExceptionHandlingData(result.data.exceptionList);
     }
-    getExceptionHandling();
 
-  }, []); 
+    getExceptionHandling();
+  }, []);
+  
+  useEffect(()=>{
+    if(resettingRef.current){
+      console.log("exceptionHandlingData useEffect!");
+      let todayInfo = {
+        sabun: window.localStorage.getItem("sabun"),
+        today: -1,
+        yoil: moment().day()
+      };
+  
+      const getWeekHistory = async () => {
+        setTimeGapArray([]);  // 초기화 
+        let result = await axios.post(BACK_END_URL+"/main/workhistory",todayInfo);
+        console.log("result >> ", result.data);
+        setUserName({name:result.data.weekHistory[0].name, sabun: window.localStorage.getItem("sabun")});
+        let wswc = [];
+        let history = result.data.weekHistory;   
+        for(let index = 0 ; index < history.length; index++){
+          let element = {wstime:"", wctime:"", workdate:""};
+          if(history[index].wstime){  
+            element.wstime = transferPrettyTimeFormatted(getTimeOnly(history[index].wstime));
+          }else{
+            element.wstime = await getCoreTimeViolationStamp({name:history[0].name, theDay:history[index].workdate, violationType:"start"});
+          }
+          if(history[index].wctime){
+            element.wctime = transferPrettyTimeFormatted(getTimeOnly(history[index].wctime));
+          }else{
+            element.wctime = await getCoreTimeViolationStamp({name:history[0].name, theDay:history[index].workdate, violationType:"end"});
+          }
+          element.workdate = history[index].workdate;
+          wswc.push(element);
+        }
+        wswc.reverse();
+        setWorkHistory(wswc);
+        for(let index =0 ; index < wswc.length ; index++){
+          let startTime = wswc[index].wstime;
+          let endTime = wswc[index].wctime;
+          let workdate = wswc[index].workdate;
+    
+          if(startTime != "기록없음" && endTime != "기록없음"){
+            let startHour = Number(startTime.split(":")[0]);
+            let startMinute = Number(startTime.split(":")[1]);
+            let endHour = Number(endTime.split(":")[0]);
+            let endMinute = Number(endTime.split(":")[1]);
+            let totalStartMin = startHour * 60 + startMinute;
+            let totalEndMin = endHour * 60 + endMinute;
+            let timeGap = totalEndMin - totalStartMin -540;
+            let filtered = exceptionHandlingData.filter(exhData => {
+              return exhData.workdate == workdate
+            })
+            console.log(" filtered >>> ", filtered);
+            if(filtered.length > 0){
+              filtered.forEach(each =>{
+                if(each.handlingException == "fd"){   // 패밀리 데이
+                  console.log(" timeGap before >> ", timeGap);
+                  timeGap = timeGap + 120;   //2시간
+                  console.log(" timeGap after >> ", timeGap);
+                }else if(each.handlingException == "hoff"){  // 반차
+                  timeGap = timeGap + 240;   // 4시간 
+                }else if(each.handlingException == "off"){   // 연차
+                  timeGap = timeGap + 540;   // 9시간
+                }else if(each.handlingException == "bt"){    // 출장, 외근
+  
+                }
+              })
+            }   
+            setTimeGapArray(prev => [...prev, timeGap]);
+          }else{
+            setTimeGapArray(prev => [...prev, "X"]);
+          }
+        }
+      }
+      getWeekHistory();
+    }
+
+  },[exceptionHandlingData]);
 
   const getBtnUx = (gubun) => {
     let ux = []
@@ -207,6 +247,7 @@ const Ham = () => {
     }
     return ux;
   }
+
 
   const changeGubunToCode = (gubun) => {
     let code = "";
@@ -230,12 +271,14 @@ const Ham = () => {
       sabun: userName.sabun,
       handlingException: changeGubunToCode(gubun) 
     }
-    console.log(" exceptionHandling >> ", exceptionHandling);
+
     let addResult = await axios.post(BACK_END_URL+"/main/addExceptionHandling", exceptionHandling);
     console.log("addExceptionHandling result >> ", addResult);
+    
     if(addResult.data.ok){
       let getResult = await axios.post(BACK_END_URL+"/main/getExceptionHandling",{todayYoil:moment().day(), sabun:userName.sabun});
       console.log(" getResult >> ", getResult.data);
+      
       let exYoil = getResult.data.exceptionList.map(each => {
         return moment(each.workdate).day()-1;
       });
@@ -243,11 +286,11 @@ const Ham = () => {
       let exType = getResult.data.exceptionList.map(each => {
         return each.handlingException;
       });
-
+      resettingRef.current = true;
       setExYoil(exYoil);
       setExType(exType);
+      setExceptionHandlingData(getResult.data.exceptionList);
     }
-
 
   };
 
