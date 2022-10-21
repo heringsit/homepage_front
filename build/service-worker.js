52fbb3f770dcc7a977698f36953274ebaaac5439
dlghgus5656 <dlghgus5656@gmail.com>
2022-10-21 16:14:41 +0900

    ham 로그인페이지 ui 수정

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0bd00a5e1a74da1362c7b04abeb50305.js"
+  "/precache-manifest.573ec61d9095af87f942945d762f9296.js"
 );
 
 self.addEventListener('message', (event) => {
