2180384b1c1c65383c590a195d207c4ffbbc627e
dlghgus5656 <dlghgus5656@gmail.com>
2024-02-26 18:29:06 +0900

    메인 이미지 수정

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f2576c4f5fb69feb95dc2f120b29cab1.js"
+  "/precache-manifest.94e08a8f1e5238256e7c4fd89dfefe34.js"
 );
 
 self.addEventListener('message', (event) => {
