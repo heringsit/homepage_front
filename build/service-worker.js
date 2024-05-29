c3f53fa11e1fd7491831cb77d77d4990f73d93c3
dlghgus5656 <dlghgus5656@gmail.com>
2024-05-29 18:54:51 +0900

    메인화면 모바일 반응 제거

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6c2ed04c3d8c20cdcdfcf2653edf1e67.js"
+  "/precache-manifest.6fbfd83d79efb841a4c979ae2fd971f6.js"
 );
 
 self.addEventListener('message', (event) => {
--- src/views/Main/Main.js
+++ src/views/Main/Main.js
@@ -66,7 +66,7 @@ export default function Main(props) {
       <Menubar slideIndex={0} />
       <Totop />
       <div className="main-container">
-        {
+        {/* {
           // 768px > 이미지; 768px < 비디오
           mTablet ? (
             <div className="mainimagelayout w-full h-full">
@@ -91,52 +91,52 @@ export default function Main(props) {
                 </span>
               </div>
             </div>
-          ) : (
-            <>
-              <div className="mainlayout">
-                <div className="maintext ">
-                  <p>
-                    <span style={{ color: "#F88001" }}>HERINGS</span> is a
-                    <br />
-                    personalized healthcare service
-                    <br /> platform company based on
-                    <br />{" "}
-                    <span style={{ color: "#F88001" }}>
-                      Remote Patient Monitoring solutions
-                    </span>
-                  </p>
-                </div>
-              </div>
-              <div className="mainlayout2">
-                <img
-                  src={main2}
-                  alt="main2"
-                  className="mainsublayout2"
-                  // style={{ height: "8vw", weight: "8vw" }}
-                  // style={{ height: "454px", weight: "300px" }}
-                />
-                <div>
-                  <p className="maintext2 ">
-                    <p
-                      style={{
-                        color: "#F68C29",
-                        fontSize: "clamp(1rem, 1.65vw, 2rem)",
-                        fontWeight: "700",
-                        textTransform: "uppercase",
-                      }}
-                    >
-                      {" "}
-                      Remote Patient Monitoring solutions
-                    </p>
-                    Every individual patient deserves heartful attention
-                    <br /> and personal care no matter where they are <br />
-                    HERINGS connects patients and medical professionals
-                    <br /> for more convenient and  immediate care through
-                    <br /> its Remote Patient Monitoring solutions
-                  </p>
-                </div>
+          ) : ( */}
+        <>
+          <div className="mainlayout">
+            <div className="maintext ">
+              <p>
+                <span style={{ color: "#F88001" }}>HERINGS</span> is a
+                <br />
+                personalized healthcare service
+                <br /> platform company based on
+                <br />{" "}
+                <span style={{ color: "#F88001" }}>
+                  Remote Patient Monitoring solutions
+                </span>
+              </p>
+            </div>
+          </div>
+          <div className="mainlayout2">
+            <img
+              src={main2}
+              alt="main2"
+              className="mainsublayout2"
+              // style={{ height: "8vw", weight: "8vw" }}
+              // style={{ height: "454px", weight: "300px" }}
+            />
+            <div>
+              <p className="maintext2 ">
+                <p
+                  style={{
+                    color: "#F68C29",
+                    fontSize: "clamp(1rem, 1.65vw, 2rem)",
+                    fontWeight: "700",
+                    textTransform: "uppercase",
+                  }}
+                >
+                  {" "}
+                  Remote Patient Monitoring solutions
+                </p>
+                Every individual patient deserves heartful attention
+                <br /> and personal care no matter where they are <br />
+                HERINGS connects patients and medical professionals
+                <br /> for more convenient and  immediate care through
+                <br /> its Remote Patient Monitoring solutions
+              </p>
+            </div>
 
-                {/* <div className="mainsublayout">
+            {/* <div className="mainsublayout">
                   {subItems.map((item, i) => (
                     <div key={i} className="mainsublayout2">
                       <img
@@ -152,112 +152,112 @@ export default function Main(props) {
                     </div>
                   ))}
                 </div> */}
-              </div>
-              <div className="mainlayoutImage mainlayout3">
-                <div className="mainsublayout3">
-                  <img src={main_sub5} alt="subImage" className="subImage" />
-                  <div className="mainsub3">
-                    <div>
-                      <p className="maintext3">
-                        The new leader in
-                        <br /> Remote Patient Monitoring
-                      </p>
-                      <p className="subContent2">
-                        Remote patient monitoring (RPM) allows doctors
-                        tomonitor, report, and analyze a patient's acute or
-                        chronic conditions while the patient is away from the
-                        hospital or clinic. Herings uses RPM-based services to
-                        enable doctors and patients to overcome distance
-                        barriers and provide therapeutic action in a short
-                        period of time in any location.
-                      </p>
-                    </div>
-                    <div
-                      className="moveButton"
-                      onClick={() => history.push("/rpmsolution")}
-                    >
-                      GO TO RPM SOLUTION
-                    </div>
-                  </div>
+          </div>
+          <div className="mainlayoutImage mainlayout3">
+            <div className="mainsublayout3">
+              <img src={main_sub5} alt="subImage" className="subImage" />
+              <div className="mainsub3">
+                <div>
+                  <p className="maintext3">
+                    The new leader in
+                    <br /> Remote Patient Monitoring
+                  </p>
+                  <p className="subContent2">
+                    Remote patient monitoring (RPM) allows doctors tomonitor,
+                    report, and analyze a patient's acute or chronic conditions
+                    while the patient is away from the hospital or clinic.
+                    Herings uses RPM-based services to enable doctors and
+                    patients to overcome distance barriers and provide
+                    therapeutic action in a short period of time in any
+                    location.
+                  </p>
                 </div>
-                <div className="mainsublayout3 itemRight">
-                  <div className="mainsub3">
-                    <div>
-                      <p className="maintext3">Want more news from HERINGS?</p>
-                      <p className="subContent2">
-                        Want more news from Herings? <br />
-                        Check out new Herings news right now!
-                        <br />
-                        We always introduce Herings with new news.
-                      </p>
-                    </div>
-                    <div
-                      className="moveButton itemRight"
-                      style={{ textAlign: "center" }}
-                      onClick={() => history.push("/news")}
-                    >
-                      GO TO News Release
-                    </div>
-                  </div>
-                  <img src={main_sub6} alt="subImage" className="subImage" />
-                </div>
-                <div className="mainlayout4">
-                  <div className="mainsub3_2">
-                    <div>
-                      <p className="maintext4">OUR MISSION</p>
-                      <p className="subContent3">
-                        Our mission is to identify the limitations of
-                        <br /> medical care in line with a rapidly developing
-                        <br /> society and implement a telemedicine
-                        <br /> platform appropriate for them.
-                      </p>
-                    </div>
-                    <div
-                      className="moveButton"
-                      onClick={() => history.push("/contactus")}
-                    >
-                      CONTACT US
-                    </div>
-                  </div>
-                  <div className="diamonds">
-                    <div className="diamond">
-                      <p className="diamond-text">IDENTIFY</p>
-                      <p className="diamond-text2">
-                        the exact pain points in
-                        <br /> current medical
-                        <br /> care system
-                      </p>
-                    </div>
-                    <div className="diamond">
-                      <p className="diamond-text">PRIORITIZE</p>
-                      <p className="diamond-text2">
-                        them according to
-                        <br /> their significance and
-                        <br /> clinical implication
-                      </p>
-                    </div>
-                    <div className="diamond">
-                      <p className="diamond-text">DEVELOP</p>
-                      <p className="diamond-text2">
-                        evidence based, science
-                        <br /> embedded solution
-                        <br /> algorithms
-                      </p>
-                    </div>
-                    <div className="diamond">
-                      <p className="diamond-text">IMPLEMENT</p>
-                      <p className="diamond-text2">
-                        them through
-                        <br /> our digital
-                        <br /> service platform.
-                      </p>
-                    </div>
-                  </div>
+                <div
+                  className="moveButton"
+                  onClick={() => history.push("/rpmsolution")}
+                >
+                  GO TO RPM SOLUTION
                 </div>
               </div>
-            </>
-          )
-        }
+            </div>
+            <div className="mainsublayout3 itemRight">
+              <div className="mainsub3">
+                <div>
+                  <p className="maintext3">Want more news from HERINGS?</p>
+                  <p className="subContent2">
+                    Want more news from Herings? <br />
+                    Check out new Herings news right now!
+                    <br />
+                    We always introduce Herings with new news.
+                  </p>
+                </div>
+                <div
+                  className="moveButton itemRight"
+                  style={{ textAlign: "center" }}
+                  onClick={() => history.push("/news")}
+                >
+                  GO TO News Release
+                </div>
+              </div>
+              <img src={main_sub6} alt="subImage" className="subImage" />
+            </div>
+            <div className="mainlayout4">
+              <div className="mainsub3_2">
+                <div>
+                  <p className="maintext4">OUR MISSION</p>
+                  <p className="subContent3">
+                    Our mission is to identify the limitations of
+                    <br /> medical care in line with a rapidly developing
+                    <br /> society and implement a telemedicine
+                    <br /> platform appropriate for them.
+                  </p>
+                </div>
+                <div
+                  className="moveButton"
+                  onClick={() => history.push("/contactus")}
+                >
+                  CONTACT US
+                </div>
+              </div>
+              <div className="diamonds">
+                <div className="diamond">
+                  <p className="diamond-text">IDENTIFY</p>
+                  <p className="diamond-text2">
+                    the exact pain points in
+                    <br /> current medical
+                    <br /> care system
+                  </p>
+                </div>
+                <div className="diamond">
+                  <p className="diamond-text">PRIORITIZE</p>
+                  <p className="diamond-text2">
+                    them according to
+                    <br /> their significance and
+                    <br /> clinical implication
+                  </p>
+                </div>
+                <div className="diamond">
+                  <p className="diamond-text">DEVELOP</p>
+                  <p className="diamond-text2">
+                    evidence based, science
+                    <br /> embedded solution
+                    <br /> algorithms
+                  </p>
+                </div>
+                <div className="diamond">
+                  <p className="diamond-text">IMPLEMENT</p>
+                  <p className="diamond-text2">
+                    them through
+                    <br /> our digital
+                    <br /> service platform.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </>
+        {/* ) */}
+        {/* } */}
       </div>
       <Footer />
     </div>
